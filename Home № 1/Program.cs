22f02e66fbf7acc330764f9c2989ff053b66a8ab
Kalininskiy Andrei <kresthaus@mail.ru>
2022-07-14 20:01:11 +0400

    Готова № 1

--- Home № 1/Program.cs
+++ Home № 1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Есть монотонная последовательность, каждое число встречается ровно то количество раз, какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+// N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+ Console.Clear();
+
+ Console.Write("Введите число N: ");
+ int countN = Convert.ToInt32(Console.ReadLine());
+ Sequence(countN);
+
+ void Sequence(int count, int number = 0, int countnum = 0){
+    if (count == 0){
+        return;
+    }
+    for (int i = 0; countnum < count; i++){
+        countnum = countnum + i;
+        number = i;
+    }
+    Sequence(--count);
+    Console.Write(number + " ");
+ } 
